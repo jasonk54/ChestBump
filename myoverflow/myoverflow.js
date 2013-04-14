4fa977a4307abe3828899bdd6b189deade3eab60
Hack Reactor Students <accounts@hackreactor.com>
2013-04-14 16:01:13 -0700

    (wip)

--- myoverflow/myoverflow.js
+++ myoverflow/myoverflow.js
@@ -1,19 +1,55 @@
-Posters = new Meteor.Collection('posters');
+Questions = new Meteor.Collection('questions');
 
 if (Meteor.isClient) {
-  Template.Names.events = {
-    'click .submitName' : function() {
-      var posterName = $('.name').val();
-      if(posterName){
-        Posters.insert({name: posterName});
-      }
-    },
-  };
 
-}
+  Meteor.Router.add({
+    '/': function() {
+      if(Session.get('name')){
+        return 'getQuestion';
+      } else {
+        return 'getName';
+      }
+    }
+  }); 
+
+  Handlebars.registerHelper('session',function(input){
+    return Session.get(input);
+  });
+
+  Template.getName.events({
+    'click .submitName' : function() {
+      // $('.greeting').hide();
+      var posterName = $('.enterName').val();
+      if(posterName) {
+        Session.set('name', posterName);
+        $('.getName').hide();
+        $('.greeting').hide();
+
+      }
+    }
+  });
+
+  Template.getQuestion.events({
+    //need to call getName
+    'click .submitQuestion' : function() {
+      var name = Session.get('name');
+      var question = $('.question').val();
+      if (question) {
+        Questions.insert({
+          text: question,
+          askerName: name
+          // need to link the user to the questions
+        })
+      }
+    }
+  });
+
+  Template.questionsList.questions = function() {
+    return Questions.find({});
+  }
+};
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
   });
 }
