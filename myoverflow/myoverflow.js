cc5706c506277945758cb887e2cadc0cf59c6a35
Hack Reactor Students <accounts@hackreactor.com>
2013-04-13 19:56:51 -0700

    Submit name fixed

--- myoverflow/myoverflow.js
+++ myoverflow/myoverflow.js
@@ -1,23 +1,15 @@
 Posters = new Meteor.Collection('posters');
 
 if (Meteor.isClient) {
-  Template.Names.name = function () {
-    var posterName = document.getElementByClass('name').value;
-    Posters.insert({name: posterName}) 
+  Template.Names.events = {
+    'click .submitName' : function() {
+      var posterName = $('.name').val();
+      if(posterName){
+        Posters.insert({name: posterName});
+      }
+    },
   };
 
-  Template.Names.getName = function () {
-    var posterName = document.getElementByClass('getName').value;
-    Posters.insert({name: posterName}) 
-  };
-
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
 }
 
 if (Meteor.isServer) {
