0d59c874799fd9af2538a0278dfb584683bf78f1
Hack Reactor Students <accounts@hackreactor.com>
2013-04-17 13:02:03 -0700

    (wip) Working on collections

--- fundme/fundme.js
+++ fundme/fundme.js
@@ -1,9 +1,40 @@
+Groups = new Meteor.Collection('groups');
+Outings = new Meteor.Collection('outings');
+
 if (Meteor.isClient) {
+  Template.users.current_user = function() {
+    return Meteor.users.find({}).fetch()
+  };
+
+  Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_AND_EMAIL'
+  })
+  // Template.users.friends = function() {};
+  // //use modal to select friends that you want to invite
+  // //Need a way to invite friends not already on the list
+
+  // Template.occasions.select_event = function() {};
+
+  // Template.occasions.planned_event = function() {};
+
+  Template.occasions.events = {
+    'click .submit_occasion' : function() {
+      var event = $('.input_occasion').val();
+      var currUser = Meteor.userId();
+      Outings.insert({
+        participant: [currUser],
+        occasion_name: event,
+        money_pool: 0, 
+        people_attended: 0
+      });
+    }
+  }
+
 
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+  // Meteor.startup(function () {
+  // });
 }
+
